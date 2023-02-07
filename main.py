7c0d0d75ee5efd5da146b062e10339441b9ff58b
Fethi Tekyaygil <fethi.tekyaygil@ciceksepeti.com>
2023-02-08 00:20:57 +0300

    Added providers as 2 columns

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ if 'address_json' not in st.session_state:
 if 'provider' not in st.session_state:
     st.session_state.provider = 'Google'
 
+if 'address_json_google' not in st.session_state:
+    st.session_state.address_json_google = ''
+
+if 'address_json_yandex' not in st.session_state:
+    st.session_state.address_json_yandex = ''
+
 
 def validate_button_clicked():
     given_address = st.session_state.address
-    st.session_state.address_json = ah.GetLocationInfo(given_address, st.session_state.provider)
+    # st.session_state.address_json = ah.GetLocationInfo(given_address, st.session_state.provider)
+    st.session_state.address_json_google = ah.GetLocationInfo(given_address, "Google")
+    st.session_state.address_json_yandex = ah.GetLocationInfo(given_address, "Yandex")
 
 
 
@@ -19,7 +27,16 @@ st.title("Address Validator")
 
 
 st.text_area("Address", key="address")
-st.selectbox("Provider:", options=["Google","Yandex"], key="provider")
+# st.selectbox("Provider:", options=["Google","Yandex"], key="provider")
 st.button("Validate", on_click=validate_button_clicked)
 
-st.write(st.session_state.address_json)
+c1,c2 = st.columns(2)
+
+with c1:
+    st.subheader("Google")
+    st.write(st.session_state.address_json_google)
+with c2:
+    st.subheader("Yandex")
+    st.write(st.session_state.address_json_yandex)
+
+
